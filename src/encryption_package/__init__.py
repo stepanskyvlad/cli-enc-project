013f327fd44382f483f68ad6ffc000a381591ac3
Vladyslav Yarema <vlad14yarema@gmail.com>
2024-03-09 00:35:01 +0200

    added default files for package

---
+++ src/encryption_package/__init__.py
@@ -0,0 +1,1 @@
+from enctool import *
---
+++ src/encryption_package/enctool.py
@@ -0,0 +1,21 @@
+"""
+Enctool module. This module contains the main functions to encrypt and decrypt texts.
+
+How to use the module:
+1) python encytool.py --encrypt path/to/binary/key/file < example_text.txt -
+    encrypts the text from example_text.txt using the binary key file and return the result
+
+2) python encytool.py --decrypt path/to/binary/key/file < example_text.txt -
+    decrypts the text from example_text.txt using the binary key file and return the result
+"""
+
+
+def main():
+    """
+    Main function to run the module.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
